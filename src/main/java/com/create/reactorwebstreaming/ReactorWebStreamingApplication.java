9eedb94eed79d2b348d292d4d19aeae5cee85a0a
create1st <sebastian.gil@gmail.com>
2018-09-12 16:17:15 +0200

    Initial commit

---
+++ src/main/java/com/create/reactorwebstreaming/ReactorWebStreamingApplication.java
@@ -0,0 +1,12 @@
+package com.create.reactorwebstreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorWebStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorWebStreamingApplication.class, args);
+	}
+}
